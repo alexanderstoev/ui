a551728f850bf549623f7d1be8aa1a34b589271e
Alexander Stoev <alex.stoev@gmail.com>
2026-04-27 11:15:51 +0300

    chore: added dark mode toggle

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,26 @@
-import type { Preview } from '@storybook/react-vite';
+import { withThemeByClassName } from "@storybook/addon-themes";
+import type { Preview } from "@storybook/react-vite";
+
 import "../src/index.css";
+
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: "", // No class for light mode
+        dark: "dark", // Adds .dark class to <html>
+      },
+      defaultTheme: "dark",
+    }),
+  ],
 };
 
 export default preview;
